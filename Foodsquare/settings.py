0c5f66f1409e3fbc1777a38ffd2abb03b967cf96
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-05-24 12:50:31 +0600

    account app module added

--- Foodsquare/settings.py
+++ Foodsquare/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'order.apps.OrderConfig',
-    'Accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
