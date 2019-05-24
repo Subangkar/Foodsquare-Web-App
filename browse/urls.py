7f8a0a54ecc95ccbb9fe99cfd55007f5c90b06d2
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-05-24 13:35:21 +0600

    base browse page loaded

---
+++ browse/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from browse import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+	path('', views.viewOrder),
+	# path('restaurants.html', views.viewRestaurants),
+	# path('index.html', views.index),
+	# path('order/', views.viewOrder),
+	# path('submitOrder/', views.submitOrder),
+	# path('allRestaurantList/', views.RestaurantList.as_view()),
+	# path('menuEntryForRestaurant/', views.RestaurantMenuEntryList.as_view()),
+	# path('branchListForRestaurant/', views.RestaurantBranchList.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
