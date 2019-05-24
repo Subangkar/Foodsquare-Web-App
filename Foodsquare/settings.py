b0793c0fefaed8e433af07a1609ee7be188392c3
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-05-24 13:10:07 +0600

    browse app added

--- Foodsquare/settings.py
+++ Foodsquare/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'accounts.apps.AccountsConfig',
+    # 'accounts.apps.AccountsConfig',
+    'browse',
+    'accounts'
 ]
 
 MIDDLEWARE = [
