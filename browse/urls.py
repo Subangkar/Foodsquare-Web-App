1ea2934122909e19dcc374762efba021f7c358a7
Subangkar <subangkar.karmaker@gmail.com>
2019-06-14 02:13:07 +0600

    Item Details & Order backend done

--- browse/urls.py
+++ browse/urls.py
@@ -5,12 +5,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
 	path('', views.Index.as_view()),
 	path('order/', views.Order.as_view()),
+	path('browse/item/', views.Item.as_view()),
 	# path('restaurants.html', views.viewRestaurants),
 	# path('browse/', views.index),
 	# path('submitOrder/', views.submitOrder),
 	# path('allRestaurantList/', views.RestaurantList.as_view()),
 	# path('menuEntryForRestaurant/', views.RestaurantMenuEntryList.as_view()),
 	# path('branchListForRestaurant/', views.RestaurantBranchList.as_view())
+	path('browse/raw/', views.viewRaw),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
