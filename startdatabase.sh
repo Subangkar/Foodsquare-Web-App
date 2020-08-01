ebb5ce57286831bdb3a5f49632a279cc9817bdf5
Subangkar <subangkar.karmaker@gmail.com>
2020-08-01 20:26:03 +0600

    Dockerised
    - initial data loading fixed

--- startdatabase.sh
+++ startdatabase.sh
@@ -1,3 +1,11 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py loaddata db.json -e contenttypes -e auth -e sessions -e admin
+if [ ! -f "/__dbloaded__" ] 
+then
+    echo "LOADING INITIAL DATA from data.json into database"
+    python3 manage.py loaddata data.json -e contenttypes -e auth -e sessions -e admin
+    touch "/__dbloaded__"
+fi
+# python3 manage.py loaddata db_int.json -e contenttypes -e auth -e sessions -e admin
+# python3 manage.py loaddata data.json -e contenttypes -e auth -e sessions -e admin
+# python3 manage.py loaddata db.json -e contenttypes -e auth -e sessions -e admin
